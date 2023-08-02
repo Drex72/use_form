bad2900ea91dba2c887b321b2827c743d5b975bf
Drex <davidokunoye003@gmail.com>
2023-08-02 23:07:55 +0100

    final commit

--- index.d.ts
+++
@@ -1,7 +0,0 @@
-export type FormError<T extends object> = {
-  [Prop in keyof T]: string | null;
-};
-
-export type Validator<T extends object> = {
-  [Prop in keyof T]?: (arg: T[Prop]) => string | null;
-};
